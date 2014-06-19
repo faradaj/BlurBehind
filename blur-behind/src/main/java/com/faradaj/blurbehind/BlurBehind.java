08a4ae64ef37313a8785b9a68bcb37ba673e99bd
Gokberk Ergun <faradaj@gmail.com>
2014-06-19 17:44:41 +0300

    Reformatted code.

--- blur-behind/src/main/java/com/faradaj/blurbehind/BlurBehind.java
+++ blur-behind/src/main/java/com/faradaj/blurbehind/BlurBehind.java
@@ -11,11 +11,11 @@ import com.faradaj.blurbehind.util.Blur;
 
 public class BlurBehind {
 
-	private static final String KEY_CACHE_BLURRED_BACKGROUND_IMAGE = "KEY_CACHE_BLURRED_BACKGROUND_IMAGE";
-	private static final int CONSTANT_BLUR_RADIUS = 16;
-	private static final int CONSTANT_DEFAULT_ALPHA = 100;
+    private static final String KEY_CACHE_BLURRED_BACKGROUND_IMAGE = "KEY_CACHE_BLURRED_BACKGROUND_IMAGE";
+    private static final int CONSTANT_BLUR_RADIUS = 16;
+    private static final int CONSTANT_DEFAULT_ALPHA = 100;
 
-	private static final LruCache<String, Bitmap> mImageCache = new LruCache<String, Bitmap>(1);
+    private static final LruCache<String, Bitmap> mImageCache = new LruCache<String, Bitmap>(1);
     private static CacheBlurBehindAndExecuteTask cacheBlurBehindAndExecuteTask;
 
     private int mAlpha = CONSTANT_DEFAULT_ALPHA;
@@ -29,8 +29,9 @@ public class BlurBehind {
     private State mState = State.READY;
 
     private static BlurBehind mInstance;
-    public static BlurBehind getInstance () {
-        if (mInstance == null){
+
+    public static BlurBehind getInstance() {
+        if (mInstance == null) {
             mInstance = new BlurBehind();
         }
         return mInstance;
@@ -54,53 +55,53 @@ public class BlurBehind {
         return this;
     }
 
-	public void setBackground(Activity activity) {
+    public void setBackground(Activity activity) {
         if (mImageCache.size() != 0) {
             BitmapDrawable bd = new BitmapDrawable(activity.getResources(), mImageCache.get(KEY_CACHE_BLURRED_BACKGROUND_IMAGE));
             bd.setAlpha(mAlpha);
-            if(mFilterColor != -1) {
+            if (mFilterColor != -1) {
                 bd.setColorFilter(mFilterColor, PorterDuff.Mode.DST_ATOP);
             }
             activity.getWindow().setBackgroundDrawable(bd);
             cacheBlurBehindAndExecuteTask = null;
         }
-	}
+    }
 
-	private class CacheBlurBehindAndExecuteTask extends AsyncTask<Void, Void, Void> {
-		private Activity activity;
-		private Runnable runnable;
+    private class CacheBlurBehindAndExecuteTask extends AsyncTask<Void, Void, Void> {
+        private Activity activity;
+        private Runnable runnable;
 
         private View decorView;
-		private Bitmap image;
+        private Bitmap image;
 
-		public CacheBlurBehindAndExecuteTask(Activity a, Runnable r) {
-			activity = a;
-			runnable = r;
-		}
+        public CacheBlurBehindAndExecuteTask(Activity a, Runnable r) {
+            activity = a;
+            runnable = r;
+        }
 
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
 
             decorView = activity.getWindow().getDecorView();
             decorView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_LOW);
             decorView.setDrawingCacheEnabled(true);
             decorView.buildDrawingCache();
 
-			image = decorView.getDrawingCache();
-		}
+            image = decorView.getDrawingCache();
+        }
 
-		@Override
-		protected Void doInBackground(Void... params) {
-			Bitmap blurredBitmap = Blur.apply(activity, image, CONSTANT_BLUR_RADIUS);
-			mImageCache.put(KEY_CACHE_BLURRED_BACKGROUND_IMAGE, blurredBitmap);
+        @Override
+        protected Void doInBackground(Void... params) {
+            Bitmap blurredBitmap = Blur.apply(activity, image, CONSTANT_BLUR_RADIUS);
+            mImageCache.put(KEY_CACHE_BLURRED_BACKGROUND_IMAGE, blurredBitmap);
 
-			return null;
-		}
+            return null;
+        }
 
-		@Override
-		protected void onPostExecute(Void aVoid) {
-			super.onPostExecute(aVoid);
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
 
             decorView.destroyDrawingCache();
             decorView.setDrawingCacheEnabled(false);
